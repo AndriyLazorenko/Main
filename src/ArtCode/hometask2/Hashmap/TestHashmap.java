c8b24edb5982c445192f45e34fc135d014b0ea2d
AndriyLazorenko <andriy.lazorenko@gmail.com>
2015-04-25 09:25:26 +0300

    All

--- src/ArtCode/hometask2/Hashmap/TestHashmap.java
+++ src/ArtCode/hometask2/Hashmap/TestHashmap.java
@@ -1,6 +1,7 @@
 package ArtCode.hometask2.Hashmap;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Master on 24-Apr-15.
@@ -10,8 +11,17 @@ public class TestHashmap {
         HashMap<Integer, String> map = new HashMap<>();
         map.put(120, "ababab");
         map.put(120, "abababa");
-        System.out.println(map.size());
+        map.put(60, "a");
         System.out.println(map.isEmpty());
+        System.out.println(map.size());
+        for(Object o:map.entrySet())
+            System.out.println(o);
+        for(Object o:map.keySet())
+            System.out.println(o);
+        for(Object o:map.values())
+            System.out.println(o);
         System.out.println(map.get(120));
+        System.out.println(map.get(60));
+        //System.out.println(map.get(120));
     }
 }
--- src/ArtCode/hometask2/Hashmap/TestMyHashmap.java
+++ src/ArtCode/hometask2/Hashmap/TestMyHashmap.java
@@ -1,6 +1,5 @@
 package ArtCode.hometask2.Hashmap;
 
-import java.util.HashMap;
 
 /**
  * Created by Lazorenko on 23.04.2015.
@@ -10,8 +9,17 @@ public class TestMyHashmap {
         MyHashMap<Integer, String> map = new MyHashMap<>();
         map.put(120, "ababab");
         map.put(120, "abababa");
-        System.out.println(map.keySet().isEmpty());
-        //System.out.println(map.size());
+        map.put(60, "a");
+        System.out.println(map.isEmpty());
+        System.out.println(map.size());
+        for(Object o:map.entrySet())
+            System.out.println(o);
+        for(Object o:map.keySet())
+            System.out.println(o);
+        for(Object o:map.values())
+            System.out.println(o);
+        System.out.println(map.get(120));
+        System.out.println(map.get(60));
         //System.out.println(map.isEmpty());
         //System.out.println(map.get(120));
     }
