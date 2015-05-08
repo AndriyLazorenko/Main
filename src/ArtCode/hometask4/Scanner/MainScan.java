d4ae1a745cb44395e95a3a9b3d0c3d7636d29cf6
AndriyLazorenko <andriy.lazorenko@gmail.com>
2015-05-08 17:55:29 +0300

    All

--- src/ArtCode/hometask4/Scanner/MainScan.java
+++ src/ArtCode/hometask4/Scanner/MainScan.java
@@ -4,4 +4,8 @@ package ArtCode.hometask4.Scanner;
  * Created by Lazorenko on 07.05.2015.
  */
 public class MainScan {
+    public static void main(String[] args) {
+        Scammer sc = new Scammer("ababa lolo ku-ku 98765 sdlcm 312 mxmzodk ui ui avciou coo-coo Motherfucker ");
+
+    }
 }
--- src/ArtCode/hometask4/Scanner/Scammer.java
+++ src/ArtCode/hometask4/Scanner/Scammer.java
@@ -1,7 +1,55 @@
 package ArtCode.hometask4.Scanner;
 
+import java.io.Reader;
+import java.util.Iterator;
+
 /**
  * Created by Master on 08-May-15.
  */
-public class Scammer {
+public class Scammer implements Iterator<String>{
+    Iterator iter = new Iterator();
+    String inputString;
+    Reader inputReader;
+    public Scammer (String str){
+        this.inputString=str;
+    }
+    public Scammer (Reader rd){
+        this.inputReader = rd;
+    }
+
+    public String next(){
+        String delimiter = " ";
+        int counterEnd=iter.iteratorNext(inputString,delimiter);
+        String forRet = inputString.substring(
+        return forRet;
+    }
+
+    public int nextInt(){
+
+    return 0;
+    }
+    public String nextLine(){
+
+    return null;
+    }
+    public boolean hasNext(){
+
+    return false;
+    }
+    public boolean hasNextInt(){
+
+    return false;
+    }
+    public void useDelimiter(String s){
+
+    }
+    public void close(){
+
+    }
+    class Iterator{
+        int iteratorPosition = 0;
+        public int iteratorNext(String str, String delimiter){
+            return iteratorPosition = str.indexOf(delimiter,iteratorPosition);
+        }
+    }
 }
